f9f0be3b928d92de29e26081524e7cb3cdf3718a
Sudeep-Thapa <sudeepthapaofficial@gmail.com>
2021-05-08 12:56:59 +0545

    worked on models

---
+++ home/models.py
@@ -0,0 +1,58 @@
+from django.db import models
+
+# Create your models here.
+STATUS=(('active','Active'),('inactive','Inactive'))
+LABEl=(('new','New'),('old','Old'),('','Default'))
+class Category(models.Model):
+    name=models.CharField(max_length=200)
+    logo=models.CharField(max_length=200)
+    slug=models.CharField(max_length=300)
+    status=models.CharField(choices= STATUS,max_length=200)
+    def __str__(self):
+        return self.name
+
+class Slider(models.Model):
+    name = models.CharField(max_length=200)
+    image=models.ImageField(upload_to='media')
+    description=models.TextField()
+    status = models.CharField(choices=STATUS, max_length=200)
+
+    def __str__(self):
+        return self.name
+
+class Ad(models.Model):
+    name = models.CharField(max_length=300)
+    image = models.ImageField(upload_to='media')
+    description = models.TextField(blank=True)
+    rank=models.IntegerField()
+    status = models.CharField(choices=STATUS, max_length=200)
+    def __str__(self):
+        return self.name
+class Brand(models.Model):
+    name = models.CharField(max_length=300)
+    image = models.ImageField(upload_to='media')
+    status = models.CharField(choices=STATUS, max_length=200)
+    def __str__(self):
+        return self.name
+class Item(models.Model):
+    name = models.CharField(max_length=300)
+    price=models.IntegerField()
+    discounted_price=models.IntegerField(default=0)
+    image = models.ImageField(upload_to='media')
+    category =models.ForeignKey(Category,on_delete= models.CASCADE)
+    brand= models.ForeignKey(Brand,on_delete=models.CASCADE)
+    label=models.CharField(choices=LABEl,max_length=200,blank=True)
+    status = models.CharField(choices=STATUS, max_length=200)
+    description = models.TextField(blank=True)
+    specification=models.TextField(blank=True)
+    slug = models.CharField(max_length=300)
+
+    def __str__(self):
+        return self.name
+
+
+
+
+
+
+
